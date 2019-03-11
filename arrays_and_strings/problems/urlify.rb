bcafbb7137cf719af7bf74098584eb7a6ab99b95
Patrick Karsh <ptkarsh@gmail.com>
2019-03-10 21:04:58 -0400

    added 1.3 urlify

---
+++ arrays_and_strings/problems/urlify.rb
@@ -0,0 +1,12 @@
+# replace all spaces in a string with %20
+
+def urlify(string)
+  string.strip!
+  string.gsub(" ", "%20")
+end
+
+# wow this was way easier than in Java..
+sample_string = "mr john smith" # excpect "mr%20john%20smith"
+sample_string_2 = "bob jones           " # expect bob%20jones
+p urlify(sample_string)
+p urlify(sample_string_2)
