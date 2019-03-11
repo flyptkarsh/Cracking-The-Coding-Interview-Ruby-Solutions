1742d7ae761b599c8f96a052dda6f5547e105fa4
Patrick Karsh <ptkarsh@gmail.com>
2019-03-11 19:15:39 -0400

    added stacks and quest

--- recursion_and_dynamic_programing/problems/recursive_multiply.rb
+++ recursion_and_dynamic_programing/problems/recursive_multiply.rb
@@ -2,4 +2,15 @@
 # Write a recursive function to multiply two positive integers without using
 # the * or / operator. You can use addtion and subtraction.
 
+# simple brute force solution
+
+def multiply_recusive(x, y)
+  if y == 0
+    return 0
+  elsif y > 0
+    return (x + multiply_recusive(x, y -1))
+  end
+end
+
 # Solution 1
+# Something with bit manipulation
