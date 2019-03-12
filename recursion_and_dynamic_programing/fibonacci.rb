a34a086e2d7bc1576f8d570ff9b41d2907f6e192
Patrick Karsh <ptkarsh@gmail.com>
2019-03-11 20:01:48 -0400

    added 8.5

--- recursion_and_dynamic_programing/fibonacci.rb
+++ recursion_and_dynamic_programing/fibonacci.rb
@@ -1,8 +1,40 @@
 #fibonacci numbers pg.131
 
 # calculate the nth fibonacci number recursively
-def fibonacci(nth)
-  return 0 if nth == 0
-  return 1 if nth == 1
-  return fibonacci(nth - 1) + fibonacci(nth - 2)
+def fibonacci_recursive_basic(n)
+  return 0 if n == 0
+  return 1 if n == 1
+  return fibonacci_recursive_basic(n - 1) + fibonacci_recursive_basic(n - 2)
 end
+
+# calculate the nth fibonacci number with memoization
+
+# bad but working solution
+def fibonacci(n)
+  if (n <= 1)
+    return n
+  end
+  fibonacci(n-1) + fibonacci(n-2)
+end
+
+def fib_store(n)
+  fib_arr = []
+  (0..n).each {|x| fib_arr << fibonacci(x)}
+  fib_arr
+end
+
+p fib_store(5)
+
+# solution that effectively uses memoization and recursion
+@fib_hash = {}
+
+def better_fibonacci(n)
+  @fib_hash[n] =
+    if n <= 1
+      n
+    else
+      @fib_hash[n] ||= better_fibonacci(n-1) + better_fibonacci(n-2)
+    end
+end
+
+p better_fibonacci(1000)
