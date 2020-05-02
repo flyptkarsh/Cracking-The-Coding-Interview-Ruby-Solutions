b9a2976850b3784a7f8e93e02e1a57c322558686
Patrick Karsh <ptkarsh@gmail.com>
2020-05-02 13:50:52 -0400

    01 knapsack change

--- recursion_and_dynamic_programing/problems/knapsack_problem_dynamic_programing.rb
+++ recursion_and_dynamic_programing/problems/knapsack_problem_dynamic_programing.rb
@@ -1,14 +1,46 @@
-class KnapsackProblem
+# https://www.youtube.com/watch?v=xOlhR_2QCXY
+# Based on a CS DOJO video
+# recursive Knapsack No DP..
 
-  def initialize(bag_capacity, items_list)
-    @capacity = bag_capacity
-    @items = items_list
+def rec_ks(weights, values, capacity)
+  @weights = w
+  @vals = v
+
+  def ks(pointer, capacity)
+    if pointer == 0 || capacity == 0
+      result = 0
+    elsif @weights[pointer] > capacity
+      result = ks(pointer-1, capacity)
+    else
+      tmp1 = ks(pointer-1, capacity)
+      tmp2 = @vals[pointer] + ks(pointer-1, capacity - @weights[pointer])
+      result = [tmp1, tmp2].max
+    end
+    result
   end
 
-  def subsolutions_matrix
-  end 
+  ks(@vals.length + 1, capacity)
+end
 
 
-
-
+# add memoization
+def rec_ks_with_memoization(weights, values, capacity)
+  @weights = w
+  @vals = v
+  @memo = []
+  def ks(pointer, capacity)
+    return @memo[pointer][capacity] if @memo[pointer][capacity]
+    if pointer == 0 || capacity == 0
+      result = 0
+    elsif @weights[pointer] > capacity
+      result = ks(pointer-1, capacity)
+    else
+      tmp1 = ks(pointer-1, capacity)
+      tmp2 = @vals[pointer] + ks(pointer-1, capacity - @weights[pointer])
+      result = [tmp1, tmp2].max
+      @memo[pointer][capacity] = result
+    end
+    result
+  end
+  ks(@vals.length + 1, capacity)
 end
